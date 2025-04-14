eaffef030f9e026449a8e8f2b891e96968da5c33
seannn9 <naeskirlu9@gmail.com>
2025-04-14 14:26:13 +0800

    removed comment

--- resources/views/refresh.blade.php
+++ resources/views/refresh.blade.php
@@ -197,6 +197,5 @@
 }
 </style>
 
-<!-- Make sure these are in your layout file -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
